ba7ac619432d8a9e5ebc4e7d30fcdbebb3b4a68b
xpCRYSISqx <mikel.gozaloqu@elorrieta-errekamari.com>
2019-10-22 08:42:29 +0200

    Modificada la clase para leer archivos

--- src/main/java/modelo/LectorArchivos.java
+++ src/main/java/modelo/LectorArchivos.java
@@ -1,4 +1,29 @@
 package modelo;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 public class LectorArchivos {
+	
+	public String leerArchivoTXT(String nombreArchivo) {
+		String path = "ficheros" + File.separator + nombreArchivo;;
+		BufferedReader buffer = null;
+		String resultado = "";
+		try {
+			buffer = new BufferedReader(new FileReader(path));
+			String linea = "";
+			while ((linea = buffer.readLine()) != null) {
+				resultado = resultado+linea +"\n";					
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			JOptionPane.showMessageDialog(new JFrame(), e.getMessage(), "Error",JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
+		}
+		return resultado;
+	}
 }
