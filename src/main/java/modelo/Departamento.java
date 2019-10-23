e2f7ad3ec32377fdcc38e9423aca998edc170a4b
xpCRYSISqx <mikel.gozaloqu@elorrieta-errekamari.com>
2019-10-23 12:35:15 +0200

    Intento de continuar cosas dsin sentido

--- src/main/java/modelo/Departamento.java
+++ src/main/java/modelo/Departamento.java
@@ -1,5 +1,36 @@
 package modelo;
 
 public class Departamento {
+	
+	private int codDepartamento;
+	private String nombre;
+	private String localizacion;
+	
+	public Departamento() {
+		
+	}
 
+	public int getCodDepartamento() {
+		return codDepartamento;
+	}
+
+	public void setCodDepartamento(int codDepartamento) {
+		this.codDepartamento = codDepartamento;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getLocalizacion() {
+		return localizacion;
+	}
+
+	public void setLocalizacion(String localizacion) {
+		this.localizacion = localizacion;
+	}
 }
