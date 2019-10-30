0282313a39981690adba934a737f9a1b7546c2df
xpCRYSISqx <mikel.gozaloqu@elorrieta-errekamari.com>
2019-10-30 13:43:47 +0100

    Comentarios y documentacion agregada el la chase de escritura de logs

--- src/main/java/modelo/EscritorFicheros.java
+++ src/main/java/modelo/EscritorFicheros.java
@@ -8,17 +8,25 @@ import javax.swing.JOptionPane;
 
 public class EscritorFicheros {
 	
+	/**
+	 * 
+	 * Metodo utilizado para generar logs de excepciones del sistema
+	 * 
+	 * @param fecha Fecha en la que ha ocurrido el error
+	 * @param motivo Motivo por el cual a ocurrido el error
+	 */
 	public void crearLog(Date fecha, String motivo) {
-		String ruta = "ficheros" + File.separator + "log.txt";
+		String ruta = "ficheros" + File.separator + "log.txt"; //Guarda la ruta del fichero
 		FileWriter fichero = null;
 		PrintWriter escritor = null;
 		try {
-			File log = new File(ruta);
+			File log = new File(ruta); //Busca si existe el fichero en la ruta especificada
 			if(!log.exists())
 				JOptionPane.showMessageDialog(new JFrame(), "Fichero log.txt no encontrado.", null, 0);
-			fichero = new FileWriter(ruta, true);
-			escritor = new PrintWriter(fichero);
+			fichero = new FileWriter(ruta, true); //Invoca FileWriter para la ruta especificada, con el true le indicamos que no borre el contedino del fichero, simplemente escrivira a continuacion del contenido actual del fichero
+			escritor = new PrintWriter(fichero); //Invoca PrintWriter en el fichero que le especificamos y de la manera que le hemos indicado con FileWriter
 			
+			//Se escribe en el archivo
 			escritor.println();
 			escritor.println("===================================================================================================================");
 			escritor.println("Fecha: " + fecha + " - Motivo del error: " + motivo);
@@ -26,7 +34,7 @@ public class EscritorFicheros {
 		catch(Exception e){
 			e.printStackTrace();
 		}
-		finally {
+		finally { //Una vez terminado, se cierra el fichero
 			try {
 				fichero.close();
 			}
