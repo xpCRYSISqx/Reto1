0348a3b9a5ab5b9ed89e0bf0e5501d69a4dc0e54
laurimoreni <laura.morenogo@elorrieta-errekamari.com>
2019-11-04 08:40:06 +0100

    mas vistas y css

--- src/main/java/vista/CardEmpleado.java
+++ src/main/java/vista/CardEmpleado.java
@@ -2,46 +2,29 @@ package vista;
 
 import java.io.IOException;
 import java.net.URL;
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
 
-import com.jfoenix.controls.JFXButton;
-import com.jfoenix.effects.JFXDepthManager;
-
-import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
-import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.paint.Paint;
 import javafx.scene.text.Text;
-import javafx.scene.web.WebView;
 import javafx.util.Duration;
 import modelo.Empleado;
 
 public class CardEmpleado extends AnchorPane implements Initializable {
 	
     @FXML
-    private AnchorPane paneBase;
+    private AnchorPane paneBase, card;
 
     @FXML
-    private AnchorPane card;
+    private Text nombre, departamento;
 
     @FXML
-    private Text nombre;
-
-    @FXML
-    private Text departamento;
-
-    @FXML
-    private Label descripcion;
+    private Label cargo;
     
     private Empleado empleado;
     
@@ -52,7 +35,7 @@ public class CardEmpleado extends AnchorPane implements Initializable {
 		this.empleado = empleado;
 		this.tAnimacion = tAnimacion;
 		
-		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/vista/empleadp.fxml"));
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/vista/empleado.fxml"));
 	    fxmlLoader.setRoot(this);
 	    fxmlLoader.setController(this);
 
@@ -70,6 +53,7 @@ public class CardEmpleado extends AnchorPane implements Initializable {
 	public void initialize(URL location, ResourceBundle resources) {		
 		this.nombre.setText(this.empleado.getNombre());
 //		this.departamento.setText(this.empleado.getCodDepartamento());
+//		this.cargo.setText(this.empleado.getCodCargo());
 //		this.descripcion.setText(this.empleado.getDescripcion());
 //		this.precio.setText(precio + "€");
 
