c964839b1176d6c2b47a63607d6f1325ba30782f
unknown <Elorrieta Errekamari@INFP4A101-24.H015112.NET>
2019-11-12 09:36:54 +0100

    merge conflict

--- src/main/java/controlador/DepartamentoControlador.java
+++ src/main/java/controlador/DepartamentoControlador.java
@@ -11,7 +11,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
+import modelo.Cargo;
 import modelo.Departamento;
+import modelo.Empleado;
 
 public class DepartamentoControlador extends Controlador implements Initializable  {
 	
@@ -30,7 +32,12 @@ public class DepartamentoControlador extends Controlador implements Initializabl
 	public void initialize(URL arg0, ResourceBundle arg1) {
 
 		Platform.runLater(() -> {
-
+			int posicion = buscarDepartamentoArray(departamento);
+			if (posicion != -1) {
+				mostrarDatosDepart(posicion);
+			} else {
+				mostrarMensaje(panelDepartamento, "Departamento no encontrado");
+			}
 
 		});
 
@@ -42,22 +49,54 @@ public class DepartamentoControlador extends Controlador implements Initializabl
 
 	@FXML
 	void anterior(ActionEvent event) {
-
+		int posicion = buscarDepartamentoArray(departamento);
+		if (posicion != -1 && posicion > 0) {
+			posicion--;
+			this.departamento = modelo.departamentos.get(posicion);
+			mostrarDatosDepart(posicion);
+		} else {
+			mostrarMensaje(panelDepartamento, "Este es el primer departamento");
+		}
 	}
 
 	@FXML
 	void primero(ActionEvent event) {
-
+		this.departamento = modelo.departamentos.get(0);
+		mostrarDatosDepart(0);
 	}
 
 	@FXML
 	void siguiente(ActionEvent event) {
-
+		int posicion = buscarDepartamentoArray(departamento);
+		if (posicion != -1 && posicion < modelo.departamentos.size() - 1) {
+			posicion++;
+			this.departamento = modelo.departamentos.get(posicion);
+			mostrarDatosDepart(posicion);
+		} else {
+			mostrarMensaje(panelDepartamento, "Este es el ultimo departamento");
+		}
 	}
 
 	@FXML
 	void ultimo(ActionEvent event) {
-
+		this.departamento = modelo.departamentos.get(modelo.departamentos.size() - 1);
+		mostrarDatosDepart(modelo.empleados.size() - 1);
+	}
+	
+	public int buscarDepartamentoArray(Departamento departamento) {
+		for(int i = 0; i < modelo.departamentos.size(); i++) {
+			if (modelo.departamentos.get(i).getCodDepartamento() == departamento.getCodDepartamento()) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public void mostrarDatosDepart(int posicion) {
+		
+		//this.departamento.setText(departamento.getNombre());
+		this.codigo.setText(String.valueOf(departamento.getCodDepartamento()));
+		this.localizacion.setText(departamento.getLocalizacion());
 	}
 
 }
