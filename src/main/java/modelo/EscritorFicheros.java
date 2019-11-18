c0bc70ebf7d6df253e2f01e2ee16a3bf0efc1aff
xpCRYSISqx <46441467+xpCRYSISqx@users.noreply.github.com>
2019-11-18 15:35:37 +0100

    Metodo de impresion de los departamentos hecho

--- src/main/java/modelo/EscritorFicheros.java
+++ src/main/java/modelo/EscritorFicheros.java
@@ -96,7 +96,7 @@ public class EscritorFicheros {
 			}
 		}
 	}
-	public void imprimirDepartamentos(ArrayList<Departamento> departamentos, ArrayList<Empleado> empleados, ArrayList<Cargo> cargos) {
+	public void imprimirDepartamentos(ArrayList<Departamento> departamentos) {
 		String ruta = "ficheros" + File.separator + "volcadoBBDD.txt"; //Guarda la ruta del fichero
 		FileWriter fichero = null;
 		PrintWriter escritor = null;
@@ -107,30 +107,10 @@ public class EscritorFicheros {
 			fichero = new FileWriter(ruta); //Invoca FileWriter para la ruta especificada, con el true le indicamos que no borre el contedino del fichero, simplemente escrivira a continuacion del contenido actual del fichero
 			escritor = new PrintWriter(fichero); //Invoca PrintWriter en el fichero que le especificamos y de la manera que le hemos indicado con FileWriter
 			
-			for(int i = 0; i < empleados.size(); i++) {
-				escritor.println("Nombre: " + empleados.get(i).getNombre());
-				escritor.println("Apellidos: " + empleados.get(i).getApellidos());
-				escritor.println("Código: " + empleados.get(i).getCodEmpleado());
-				escritor.println("Sueldo: " + empleados.get(i).getSueldo() + "€");
-				for(int j = 0; i < departamentos.size(); j++) {
-					if(empleados.get(i).getCodDepartamento() == departamentos.get(j).getCodDepartamento())
-						escritor.println("Departamento: " + departamentos.get(j).getNombre());
-				}
-				for(int j = 0; j < empleados.size(); j++) {
-					if(empleados.get(i).getCodJefe() == empleados.get(j).getCodJefe())
-						escritor.println("Código del jefe: " + empleados.get(j).getNombre());
-					else
-						escritor.println("Código del jefe: No tiene jefe");
-				}
-				for(int j = 0; j < cargos.size(); j++) {
-					if(empleados.get(i).getCodCargo() == cargos.get(j).getCodCargo())
-						escritor.println("Código del cargo: " + cargos.get(j).getNombre());
-				}
-				if(empleados.get(i).getEsJefe())
-					escritor.println("Es jefe: Si");
-				else
-					escritor.println("Es jefe: No");
-				escritor.println("Fecha de alta: " + empleados.get(i).getFechaAlta());
+			for(int i = 0; i < departamentos.size(); i++) {
+				escritor.println("Nombre del departamento: " + departamentos.get(i).getNombre());
+				escritor.println("Localización del departamento: " + departamentos.get(i).getLocalizacion());
+				escritor.println("Código del departamento: " + departamentos.get(i).getCodDepartamento());
 				escritor.println("----------------------------------------------------------------------------");
 				escritor.println();
 			}
